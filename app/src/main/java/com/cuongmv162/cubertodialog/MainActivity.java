6fb62cd33f8faf277954cd010fff88f73c382c6b
Cuong Vo <cuongmv162@gmail.com>
2017-05-16 00:43:53 +0700

    udapte

--- app/src/main/java/com/cuongmv162/cubertodialog/MainActivity.java
+++ app/src/main/java/com/cuongmv162/cubertodialog/MainActivity.java
@@ -3,11 +3,16 @@ package com.cuongmv162.cubertodialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cuongmv162.cubertodialog.dialog.CubertoDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CubertoDialog cubertoDialog = new CubertoDialog();
+        cubertoDialog.show(getFragmentManager(), "Cuberto dialog");
     }
 }
--- app/src/main/java/com/cuongmv162/cubertodialog/dialog/CubertoDialog.java
+++ app/src/main/java/com/cuongmv162/cubertodialog/dialog/CubertoDialog.java
@@ -1,9 +1,42 @@
 package com.cuongmv162.cubertodialog.dialog;
 
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.cuongmv162.cubertodialog.R;
+
 /**
  * Created by cuongmv162 on 2/1/2017.
  */
 
-public class CubertoDialog {
+public class CubertoDialog extends DialogFragment {
+    private View mView;
 
+    public CubertoDialog() {
+
+    }
+
+    public void create() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        mView = inflater.inflate(R.layout.dialog_cuberto_layout, null);
+
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        return mView;
+    }
 }
